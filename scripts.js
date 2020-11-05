8064956eaedc4e18c682cb1a26d5a7538c537479
pvl <mateescupavel@yahoo.com>
2020-11-05 21:23:10 +0200

    [UPDATE] added CSS changing jQuery script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 console.log('[OCWCandy] No %c, no shit, just for the sake of my mental health.');
+
+(function(){
+    $(document).ready(function() {
+        $("body").css({"margin":"3px"});
+        $("#wrapper").css({
+            "max-width":"100%",
+            "border":"none",
+            "padding":"none"
+        });
+        $(".left_page").css({
+            "width":"100% !important",
+            "padding":"none"
+        })
+        $(".level2").css({"margin-left":"none"});
+        $(".level3").css({"margin-left":"none"});
+        $(".level4").css({"margin-left":"none"});
+    });
+})(jQuery);
