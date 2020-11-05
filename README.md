e3692928b894fa02ea3325fd22471e8acda8fe37
pvl <mateescupavel@yahoo.com>
2020-11-06 01:26:30 +0200

    [UPDATE] finished version 0.2.0

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # OCWCandy
 A simple chrome extension for UPB students who want two windows side by side while not wasting precious space.
+
+<p align="center">
+<img src="images/comparison.png">
+</p>
+
+Left - The default window  |  Right - with OCW Candy toggled On
+
+Installation
+============
+You will need to turn developer mode on in [chrome://extensions](chrome://extensions) from top right.
+
+1. Clone OCW Candy to our system;
+2. Select the cloned repo in [chrome://extensions](chrome://extensions) in Load unpacked, top left button;
+3. Pin OCW Candy;
+
+How to use
+==========
+Go to OCW, to the Laboratory page, select your particular lab, after the page loads, toggle OCW Candy from the On button, and to stop, toggle from Off.
+
+Original Developer
+------------------
+
+ * Pavel-Vlad Mateescu - [Pvl26](https://github.com/Pvl26)
--- scripts.js
+++ scripts.js
@@ -1,26 +1,39 @@
-console.log('[OCWCandy] No %c, no shit, just for the sake of my mental health.');
-
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';    
-
-document.getElementsByTagName('head')[0].appendChild(script);
-
+console.log('[OCWCandy] No bullshit, just for the sake of my mental health.');
 
 (function(){
     $(document).ready(function() {
-        $("body").css({"margin":"3px"});
-        $("#wrapper").css({
-            "max-width":"100%",
-            "border":"none",
-            "padding":"none"
-        });
-        $(".left_page").css({
-            "width":"100% !important",
-            "padding":"none"
-        })
-        $(".level2").css({"margin-left":"none"});
-        $(".level3").css({"margin-left":"none"});
-        $(".level4").css({"margin-left":"none"});
+        document.getElementById("on").addEventListener('click', toggleon, false);
+
+        function toggleon () {
+            chrome.tabs.query({currentWindow: true, active: true},
+                function (tabs) {
+                    chrome.tabs.executeScript(tabs[0].id, {
+                        code: `$("body").css({"margin":"3px"});
+                        $("#wrapper").css({
+                            "max-width":"100%",
+                            "width":"100%",
+                            "border":"none",
+                            "padding":"0px"
+                        });
+                        $(".dokuwiki").find(".left_page").css({
+                            "cssText":"width: 100% !important; padding: 0px;"
+                        })
+                        $(".dokuwiki").find(".level2").css({"margin-left":"0px"});
+                        $(".dokuwiki").find(".level3").css({"margin-left":"0px"});
+                        $(".dokuwiki").find(".level4").css({"margin-left":"0px"});`
+                    });
+                })   
+        }
+
+        document.getElementById("off").addEventListener('click', toggleoff, false);
+
+        function toggleoff () {
+            chrome.tabs.query({currentWindow: true, active: true},
+                function (tabs) {
+                    chrome.tabs.executeScript(tabs[0].id, {
+                        code: `location.reload();`
+                    });
+                })  
+        }
     });
 })(jQuery);
